a9a4a9e4a2be6c1d7d9ad3eddfe8a5349c3f6681
austindavis65 <austinrdavis1026@gmail.com>
2026-01-12 11:32:06 -0700

    Add displaystartup.bat

---
+++ displaystartup.bat
@@ -0,0 +1,10 @@
+start chrome --new-window "https://help.utahtech.edu/staff/tickets/"
+start chrome --new-window "https://144.38.53.34/GVE/HelpDesk/HelpDesk.aspx"
+start chrome --new-window "https://cloud.mersive.com/availability"
+start chrome --new-window "https://www.youtube.com/embed/r3YdBqqDLQk?autoplay=1&modestbranding=1$controls=0&showinfo=0"
+
+timeout /t 20 /nobreak >nul
+
+start "" "C:\Program Files\Palo Alto Networks\GlobalProtect\PanGPA.exe"
+
+start "" "C:\Program Files\DisplayFusion\DisplayFusion.exe"
